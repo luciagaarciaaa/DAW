cd2a732978328cb760761ab156f4f12b521b4255
luciagaarciaaa <lucia55ee@gmail.com>
2024-09-18 13:38:35 +0200

    ACTURALIZACION

---
+++ DWECL/03_tipos/js/index.js
@@ -0,0 +1,15 @@
+let nombre="Maria", numero;
+
+//Mostrar el tipo de las variables
+
+console.log(`el tipo de la variable  ${nombre} es ${typeof nombre}`);
+console.log(`el tipo de la variable  ${numero} es ${typeof numero}`);
+
+numero=23;
+console.log(`el tipo de la variable  ${numero} es ${typeof numero}`);
+numero=true;
+console.log(`el tipo de la variable  ${numero} es ${typeof numero}`);
+numero="23";
+console.log(`el tipo de la variable  ${numero} es ${typeof numero}`);
+numero=numero+23;
+console.log(`el tipo de la variable  ${numero} es ${typeof numero}`);
---
+++ DWECL/04_condicional/js/index.js
@@ -0,0 +1,17 @@
+const numero = "123";
+
+if (numero == 123){
+    console.log("Los numeros son iguales ==");
+
+}else{
+    console.log("Los numeros no son iguales ==");
+    
+}
+
+if (numero === 123){
+    console.log("Los numeros son iguales ===");
+    
+}else{
+    console.log("Los numeros no son iguales ===");
+    
+}
