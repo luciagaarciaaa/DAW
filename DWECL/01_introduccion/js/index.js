a05d6d3654f874cdf351c638e90096c13a989db0
luciagaarciaaa <lucia55ee@gmail.com>
2024-09-18 13:10:19 +0200

    ACTUALIZADO 18/09/2024

---
+++ DWECL/01_introduccion/js/index.js
@@ -0,0 +1,8 @@
+console.log("hola consola!");
+alert("hola usuario");
+console.log(prompt("introduce un numero"));
+console.log(confirm("¿Deseas continuar?"));
+
+
+
+
---
+++ DWECL/02_variables/js/index.js
@@ -0,0 +1,8 @@
+"use strict" //Obligatorio para declarar variables y constantes
+
+let nombre="Maria";
+var apellido="Ojeda";
+const edad=20;
+
+console.log("Me llamo" + nombre + "" + apellido + "y tiene" + edad + "años."); 
+console.log(`Me llamo ${nombre} ${apellido} y tengo ${edad} años.`);
